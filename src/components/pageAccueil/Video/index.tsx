3021053dcdc347542c4d1550766f736699a5fb1f
BastienMall <bastien.mall@hotmail.fr>
2023-10-28 16:32:08 +0200

    Remplacement de la vidéo par une vidéo personnalisé

--- src/components/pageAccueil/Video/index.tsx
+++ src/components/pageAccueil/Video/index.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import ReactPlayer from 'react-player'
 
 function Video() {
+
+
     return(
         <div>
             <center>
 
             <ReactPlayer 
-            url={'https://www.youtube.com/watch?v=ZyjT2KyhHEk'}
+            url={'https://www.youtube.com/watch?v=ILv_KBeBdJs'}
             playing
             muted
             loop
