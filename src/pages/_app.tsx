245b85d779665570d3fa6991d444d809e668508c
Mahen-4 <mahen.ath@outlook.com>
2023-10-28 14:05:12 +0200

    [DONE] 98% of NavBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import WithAction from '@/components/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <WithAction />
       <Component {...pageProps} />
     </ChakraProvider>
   )
---
+++ src/pages/pageService.tsx
@@ -0,0 +1,10 @@
+//CARELAGE PEINTURE PARQUET PLOMBERIE ELECTRICITE MEUBLE REAMENAGEMENT AMENAGEMENT(creation d'une nouvelle piece) 
+
+
+const PageService = () => {
+    return(
+        <h1>Page Service</h1>
+    )
+}
+
+export default PageService
