ada14283e4f46209e7f3d25a7493af5518de77c8
Mahen-4 <mahen.ath@outlook.com>
2023-10-28 22:59:36 +0200

    [Done] service Page & nav bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import WithAction from '@/components/navbar'
+import '../styles/pageServiceStyle.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
