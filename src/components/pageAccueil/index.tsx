02fb85e9df872d029c32f3d6eab1289740bfd217
BastienMall <bastien.mall@hotmail.fr>
2023-10-28 13:45:18 +0200

    Ajout d'un carrousel

--- src/components/pageAccueil/index.tsx
+++ src/components/pageAccueil/index.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react"
 import WithSpeechBubbles from "./temoignage"
 import LargeWithLogoCentered from "../footer"
+import Carousel from "./carrousel"
 
 
 const PageAccueil = () => {
     return(
         <Box>
         <h1>ACCUEIL</h1>
+        <Carousel/>
         <WithSpeechBubbles/>
         <LargeWithLogoCentered/>
         </Box>
