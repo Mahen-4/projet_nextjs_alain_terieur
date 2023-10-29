40a69e3f02359ea42d440598f435c928b16c32c9
BastienMall <bastien.mall@hotmail.fr>
2023-10-29 17:59:28 +0100

    Implémentation du router dans le bouton 'Nos Service' pour la redirection

--- src/components/pageAccueil/index.tsx
+++ src/components/pageAccueil/index.tsx
@@ -6,9 +6,25 @@ import Video from "./Video"
 import Description from "./description"
 import Link from "next/link"
 import Pub from "./pub"
+import { useRouter } from 'next/router'
+import React from "react"
+
+
 
 
 const PageAccueil = () => {
+
+    const router = useRouter()
+
+    const [activeMenu, setActiveMenu] = React.useState(false)
+
+    const changeActiveMenu  = (menu_path: string) => {
+        router.push(menu_path)
+        setActiveMenu(!activeMenu)
+    }
+
+
+
     return(
         <Box>
         {/* <Carousel/> */}
@@ -17,7 +33,7 @@ const PageAccueil = () => {
         <WithSpeechBubbles/>
         <center>    
         <Link href={'PageService'} >
-            <Button backgroundColor='#abe0f2' size={'lg'} width={400} height={75} borderRadius={25} marginBottom={150}>
+            <Button onClick={()=>{changeActiveMenu("/pageService")}} _hover={{ bg: '#abe0f2', color: "black" }} backgroundColor= {'#f9af92'} variant='ghost' size={'lg'} width={400} height={75} borderRadius={25} marginBottom={150}>
                     Découvrez nos service !
             </Button>
         </Link>
